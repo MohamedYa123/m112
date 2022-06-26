c4f2ace5b3b6046002894b8cecd4628d033f4a46
MohamedYa123 <tecyouth123@gmail.com>
2022-06-26 09:41:36 +0200

    r

--- medo/settings.py
+++ medo/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(g4+cf9bli+ce8#cvp28#+sbnh1dzbte#4t@*f5$)bz#8)(61@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
